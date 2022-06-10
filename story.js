f3e4756902deb0cdbe8cbf7e56dc7dcf50dfd101
Josephinesps <98600503+Josephinesps@users.noreply.github.com>
2022-06-09 22:52:59 -0400

    Update story.js

--- story.js
+++ story.js
@@ -54,14 +54,14 @@ function checkAnswers(answer) {
 function sceneTest(){
   sceneNum = 1;
   addImage("StartingScreen.png");
-  story("You wake up, in a place that looks oddly familiar. Your head is pounding, you struggle to stand up and try to understand where you are. You see ahead of you what looks like a house with surpisingly a bathroom behind it.<br>What do you do?");
+  story("Scene 1<br>You wake up, in a place that looks oddly familiar. Your head is pounding, you struggle to stand up and try to understand where you are. You see ahead of you what looks like a house with surpisingly a bathroom behind it.<br>What do you do?");
   choices = ["Use the bathroom","Explore the House"];
   answer = setOptions(choices);
 }
 
 function findShrek(){
   addImage("ShrekGettingOutOfShed.png");
-  var messages = ['You pray that no one was inside as you slowly open the door and see Shrek TAKING A MASSIVE DUMP. You slammed the door in embaressment.'
+  var messages = ['Scene 3<br>You pray that no one was inside as you slowly open the door and see Shrek TAKING A MASSIVE DUMP. You slammed the door in embaressment.'
   ];
   delayText(messages, 1000);
   choices = ["Start over"];
@@ -71,34 +71,34 @@ function findShrek(){
 
 function inHouse(){
   addImage("InsideHouse.png");
-  story("You walk in the house to find Donkey sitting on a chair, He looks frightened.<br>Should we talk to him?");
+  story("Scene 5<br>You walk in the house to find Donkey sitting on a chair, He looks frightened.<br>Should we talk to him?");
   choices = ["Talk to him","Explore some more"];
   answer = setOptions(choices);
 }
 
 function bathroom(){
   addImage("ShrekShed.png");
-  story("You decide to go to the bathroom before you start your exploration, you are hesitant. It looks like there is someone in there.<br>Do you still want to use the bathroom?");
+  story("Scene 2<br>You decide to go to the bathroom before you start your exploration, you are hesitant. It looks like there is someone in there.<br>Do you still want to use the bathroom?");
   choices = ["Open Door","Go back"];
   answer = setOptions(choices);
 }
 
 function donkey(){
-  story("You say hi to Donkey and ask if he is okay.<br>Donkey hesitantly whispered he's coming...<br>What do you mean by he's coming? who's he? ...Donkey? you screamed.<br>Yet Donkey doesn't answer nor does he even bat a eye.<br> You're confused, concered even.<br>The best thing you can do is see if Shrek has any idea what is going on.");
+  story("Scene 5<br>You say hi to Donkey and ask if he is okay.<br>Donkey hesitantly whispered he's coming...<br>What do you mean by he's coming? who's he? ...Donkey? you screamed.<br>Yet Donkey doesn't answer nor does he even bat a eye.<br> You're confused, concered even.<br>The best thing you can do is see if Shrek has any idea what is going on.");
   choices = ["Go see Shrek"];
   answer = setOptions(choices);
 }
 
 function kitchen(){
   addImage("DinnerTime.png");
-  story("You see Shrek CHOWING down some food<br>Hey do you know what is up with Do-<br>CRASH!!!! HEE HAWWWWWWW!!!<br>you and Shrek both go into shock.<br> you start shaking in fear.<br>Was that Donkey?<br>Did someone just break in?<br>What do you want to do?");
+  story("Scene 6<br>You see Shrek CHOWING down some food<br>Hey do you know what is up with Do-<br>CRASH!!!! HEE HAWWWWWWW!!!<br>you and Shrek both go into shock.<br> you start shaking in fear.<br>Was that Donkey?<br>Did someone just break in?<br>What do you want to do?");
   choices = ["Eat Shreks food","Go investagate","Cry"];
   answer = setOptions(choices);
 }
 
 function goBack(){
   addImage("StartingScreen.png");
-  var messages = [ 'You got scared and decided to head back to the house<br>want to explore?'
+  var messages = ['Scene 4<br>You got scared and decided to head back to the house<br>want to explore?'
   ];
   delayText(messages, 1000);
   choices = ["Explore the House"];
@@ -107,41 +107,41 @@ function goBack(){
 
 function readingLetter(){
   addImage("letter.png");
-  story("You pick up the note and read it outloud.<br>I have your friend. If you want your friend back, you're going have to give me your swamp.<br>...oh my god! You ask Shrek what to do.<br>As Shrek explains his plan you look outside and see a nice lake of swamp water.<br>Do you want to take a dip.");
+  story("Scene 10<br>You pick up the note and read it outloud.<br>I have your friend. If you want your friend back, you're going have to give me your swamp.<br>...oh my god! You ask Shrek what to do.<br>As Shrek explains his plan you look outside and see a nice lake of swamp water.<br>Do you want to take a dip.");
   choices = ["Go find Donkey","Go for a swim"];
   answer = setOptions(choices);
 }
 
 function eatFood(){
   addImage("Eating.gif");
-  story("Shrek run out to go investagate on what happened. On you way to follow him, you look back at the table and see a feast of delicous foods.<br>Your stomatch growls, as your mouth started to water.<br>Your hunger takes over as you start to DEVOUR the food.");
+  story("Scene 7<br>Shrek run out to go investagate on what happened. On you way to follow him, you look back at the table and see a feast of delicous foods.<br>Your stomatch growls, as your mouth started to water.<br>Your hunger takes over as you start to DEVOUR the food.");
   choices = ["Start over"];
   answer = setOptions(choices);
 }
 
 function findOut(){
   addImage("letter.png");
-  story("You and Shrek both decide to go investagate.<br>You turn the corner and by your surpise you see nothing but a note.<br>Where's Donkey though?<br>Maybe the letter will help.");
+  story("Scene 9<br>You and Shrek both decide to go investagate.<br>You turn the corner and by your surpise you see nothing but a note.<br>Where's Donkey though?<br>Maybe the letter will help.");
   choices = ["Read the Letter"];
   answer = setOptions(choices);
 }
 
 function booHoo(){
   addImage("crying.gif");
-  var messages = ("You get scared to and start to cry.<br>What a BABY!!");
+  var messages = ("Scene 8<br>You get scared to and start to cry.<br>What a BABY!!");
   choices = ["Start over"];
   answer = setOptions(choices);
 }
 
 function swimmingWithShrek(){
   addImage("GoSwimming.png");
-  story("You ignore everything that Shrek said and decide to take a dip. You convice Shrek to go with you.<br>As you guys were swimming you realize how much of a nice...handsome...kindhearted guy he is.<br>You and Shrek forget about donkey and start spending more and more time togther.<br>You're in love.");
+  story("Scene 11<br>You ignore everything that Shrek said and decide to take a dip. You convice Shrek to go with you.<br>As you guys were swimming you realize how much of a nice...handsome...kindhearted guy he is.<br>You and Shrek forget about donkey and start spending more and more time togther.<br>You're in love.");
   choices = ["Start over"];
   answer = setOptions(choices);
 }
 
 function startingAdventure(){
   addImage("OntheRoad.png");
-  story("You guys are on the road on the way to the most reasonable place to look.<br>The VILLAGE.");
+  story("Scene 12<br>You guys are on the road on the way to the most reasonable place to look.<br>The VILLAGE.");
   choices = ["To be continued"];
 }
